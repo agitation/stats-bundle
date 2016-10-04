a6b82353383ffff1cad34feff0e285bd81e316d9
Alexander Günsche <mail@lxg.de>
2016-10-04 17:12:48 +0200

    event to activate data processors

---
+++ lib/Event/StatsProcessEvent.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * @package    agitation/stats-bundle
+ * @link       http://github.com/agitation/stats-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\StatsBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class StatsProcessEvent extends Event
+{
+}
